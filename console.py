9357d8673e6ac686421c6b0599670422d503913e
Marcos Paulo Gomes do Nascimento <140089234+Zatzinh0@users.noreply.github.com>
2026-06-12 19:50:41 -0300

    Capitalize user input for day of the week
    Capitalize user input for day of the week.

--- console.py
+++ console.py
@@ -2,7 +2,7 @@ dia_de_semana = ["Segunda", "Terça", "Quarta", "Quinta", "Sexta"]
 final_de_semana = ["Sábado", "Domingo"]
 # Dias de semana e final de semana adicionado em arrays
 
-dia_usuario = input("Digite o dia da semana: ")
+dia_usuario = input("Digite o dia da semana: ").capitalize()
 # Aqui perguntamos o dia da semana que o usuário está
 
 if dia_usuario in dia_de_semana:
